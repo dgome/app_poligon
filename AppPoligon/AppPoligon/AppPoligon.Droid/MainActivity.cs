63245c8e91f4353d065ebfd371e3217717619bd0
xamarin_host <xamarin_host@DESKTOP-U1UNHCP>
2016-06-22 02:56:17 +0200

    Compartir

--- AppPoligon/AppPoligon/AppPoligon.Droid/MainActivity.cs
+++ AppPoligon/AppPoligon/AppPoligon.Droid/MainActivity.cs
@@ -6,6 +6,7 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Android.Content;
 
 namespace AppPoligon.Droid
 {
@@ -15,9 +16,19 @@ namespace AppPoligon.Droid
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
+                    
 
             global::Xamarin.Forms.Forms.Init(this, bundle);
             LoadApplication(new App());
+
+            //Esto hacemos que podamos compartir cualquier texto con cualquier aplicación, nos sale el dialogo para compartir con todo
+            Intent sendIntent = new Intent();
+            sendIntent.SetAction(Intent.ActionSend);
+            sendIntent.PutExtra(Intent.ExtraText, "This is my text to send.");
+            sendIntent.SetType("text/plain");
+            StartActivity(sendIntent);
+            //////////////////////////////////////////
+
         }
     }
 }
